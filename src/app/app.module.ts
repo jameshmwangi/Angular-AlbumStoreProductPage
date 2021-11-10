3478c119226823469bef0f6fcdab7b6556ff882b
jameshmwangi <jameshmwangiy@gmail.com>
2021-11-10 21:36:22 +0300

    App first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductPageComponent,
+    ProductDescriptionComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
