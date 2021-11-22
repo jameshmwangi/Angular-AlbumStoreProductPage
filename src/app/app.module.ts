a5c0df385d5d103f274caed9f698bb85af9bb41d
jameshmwangi <jameshmwangiy@gmail.com>
2021-11-22 13:52:57 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-
+import { ProductService } from './product.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +18,7 @@ import { ProductDescriptionComponent } from './product-description/product-descr
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http:Http) { }
+ private _albumUrl: string = '../assets/album.json';
+ getAlbum(id:number){
+   return 
+ };
+}
