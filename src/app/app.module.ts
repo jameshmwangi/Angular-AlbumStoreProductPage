75081d0a8193b0af5293477c8f1fcf567ee57e14
jameshmwangi <jameshmwangiy@gmail.com>
2021-11-26 23:25:57 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductTracklistingComponent
+    ProductTracklistingComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import { Album } from './album';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 @Injectable()
 export class ProductService {
 
@@ -11,4 +12,9 @@ export class ProductService {
  getAlbum(id:number):Observable<Album>{
    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
  }
+ private _productsUrl: string = '../assets/products.json';
+ getProducts():Observable<Product[]>{
+   return this._http.get(this._productsUrl).map((response)=><Product[]>response.json());
+ }
+
 }
